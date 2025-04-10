4e77e199f328b7c75498179f18d477d98b1ffc33
Andrew Tsybulsky <andrey.tsybulski@innowise.com>
2025-04-10 12:51:09 +0000

    i need input field surround and show all text

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from 'react';
 
 import {cn} from '@/lib/utils';
@@ -6,7 +8,9 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
   ({className, ...props}, ref) => {
     return (
       <textarea
-        className={cn(
+        wrap="hard"
+        style={{overflow: 'visible'}}
+        className={cn(          
           'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
           className
         )}
@@ -14,7 +18,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
         {...props}
       />
     );
-  }
+  },
 );
 Textarea.displayName = 'Textarea';
 
